b0e2448f667c9b7d54b7fe19d57bf01b9a6a0679
Thomas Hoddinott <thomashoddinott477@gmail.com>
2021-03-23 11:32:11 +0000

    tidy up

--- tryElm/src/Main.elm
+++
@@ -1,9 +0,0 @@
-pluralize singular plural quantity =
-    if quantity == 1 then
-        singular
-        
-    else 
-        plural
-
-main = -- this is an inline comment
-    text (pluralize "leaf" "leaves" 1)
